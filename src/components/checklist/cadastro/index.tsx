50659edc24715b21db0ce7de631a3d5ac751130e
Digão Reis <digaoreis.dev@gmail.com>
2024-03-20 17:28:59 -0300

    Criando Formulário
    * Iniciei o Formulário para gerar CheckList
    
    Co-Authored-By: Adriel Garcia <adrielgarcia@live.com>

--- src/components/checklist/cadastro/index.tsx
+++ src/components/checklist/cadastro/index.tsx
@@ -3,8 +3,27 @@ import 'bulma/css/bulma.css';
 
 export const CadastroChecklist: React.FC = () => {
   return (
-    <Layout titulo="Cadastro de Checklist" >
-      Aqui vai ficar o meu formulário
+    <Layout titulo='Cadastro de Checklist' >
+      <div className='field'>
+        <label className='label' htmlFor='inputData'>Data: *</label>
+        <div className='control'>
+          <input  className='input' 
+                  id='inputData'
+                  placeholder='20/03/2024' />
+        </div>
+      </div>
+      
+      <div className='field'>
+        <label className='label' htmlFor='selectVeiculo'>Veículo: *</label>
+        <div className='control'>
+          <div className='select'>
+            <select className="is-hover" id='selectVeiculo'>
+              <option>IHY3301</option>
+              <option>ILO9010</option>
+            </select>            
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
